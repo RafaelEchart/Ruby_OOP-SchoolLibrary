849bba78a9d0cabbb01b716d3f0527a4f2c814a4
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-29 02:14:29 -0500

    Linters Tests

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Student < Person
   attr_reader :classroom
-  
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
